d92e41e5cd0bb79499f234026ec73601bbed998c
GOAT <151907116+km0808@users.noreply.github.com>
2026-05-26 23:30:52 +0200

    js
    Refactor fetch function and add random vehicle display logic.

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-async function getfech(url){
+async function getch(url){
     const response = await fetch(url);
     const json_promise = await response.json();
     return json_promise;
 }
 
-async function uj_info(){
-
-    let data = await getfech(
-        "https://starwars-databank-server.onrender.com/api/v1/vehicles"
-    );
-
-    let output = document.getElementById("output");
-
-    output.innerHTML = "";
-
-    data.data.forEach(function(vehicle){
-
-        let div = document.createElement("div");
-
-        div.innerHTML = `
-            <h2>${vehicle.name}</h2>
-            <p>Modell: ${vehicle.model}</p>
-            <p>Gyártó: ${vehicle.manufacturer}</p>
-        `;
-
-        output.appendChild(div);
-    });
+function veletlen_szam(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+async function uj_info(){
+    console.log("megnyomódtam");
+    
+    let data = await getch("https://onrender.com");
+    console.log(data);
+    console.log("adat megérkezett és már ki is írtam");
+    
+    let jarmuvek = data.data;
+    let randomelem = veletlen_szam(0, jarmuvek.length - 1);
+    
+    faj.innerHTML = jarmuvek[randomelem].name;
+    leir.innerHTML = jarmuvek[randomelem].description;
+    kep.src = jarmuvek[randomelem].image;
+}
+
+const gomb = document.getElementById("gomb");
+const faj = document.getElementById("faj");
+const kep = document.getElementById("kep");
+const leir = document.getElementById("leir");
+
+gomb.addEventListener("click", uj_info);
